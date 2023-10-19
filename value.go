adb9c2c582f1022ad508f0b59338b2a1ce5c7042
xieyuschen <xieyuschen@gmail.com>
2023-10-19 19:09:41 +0800

    chore: put value definition in a separated file

---
+++ value.go
@@ -0,0 +1,36 @@
+package astjson
+
+// NodeType represents an AST node type
+type NodeType uint
+
+const (
+	Number NodeType = iota
+	Null
+	String
+	Bool
+	Object
+	Array
+)
+
+// Value is the concrete AST representation
+type Value struct {
+	NodeType
+	
+	// AstValue stores the real value of an AST Value
+	// the literal type(Number, String, Bool and Null) stores them by value
+	// the Object and Array stores them by pointer
+	AstValue interface{}
+}
+
+type NumberAst float64
+type NullAst struct{}
+type BoolAst bool
+type StringAst string
+
+type ObjectAst struct {
+	m map[Value]Value
+}
+
+type ArrayAst struct {
+	values []Value
+}
